54c2c0aaa279828b7f0244a689d093b4ca49038b
MathieuKruk <mathieukruk@gmail.com>
2020-09-09 17:00:38 +0200

    save

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+//#############################################################################################//
+// GLOBAL CONST
 const express = require('express');
 const app = express();
 const fs = require('fs');
 
 const websiteName = 'Kruk Construction';
-const PORT = process.env.PORT || 3000;
-const host = 'localhost';
+const port = process.env.PORT || 3000;
 
-// Rendering template
+
+//#############################################################################################//
+// RENDERING TEMPLATE
 app.set("view engine", "pug");
 app.set('views', __dirname + '/src');
 // Folder hosting all static files
@@ -20,66 +23,73 @@ app.use(express.static(__dirname + '/public'));
 // 	res.render('guardian', memberDetails);
 // });
 
+
+//#############################################################################################//
 // DATA PARSING
 app.use(express.urlencoded({
 	extended: false
 }));
 app.use(express.json());
 
+
+//#############################################################################################//
 // ROUTING
+
 // Landing page
 app.get('/', function(req, res) {
-    res.render('pages/landing');
-    console.log('DEV-Message: Home/landing page has correclty loaded')
+	res.render('pages/landing');
+	console.log('%c DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  Landing page has correclty loaded.', 'color: green;')
 });
 
 // About us page
 app.get('/about', function(req, res) {
-    res.render('pages/about');
-    console.log('DEV-Message: About us page has correclty loaded')
+	res.render('pages/about');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  About us page has correclty loaded.')
 });
 
 // Achievment: insulating facades page
 app.get('/achievements/facades', function(req, res) {
-    res.render('pages/achievement-facades');
-    console.log('DEV-Message: Achievement page has correclty loaded')
+	res.render('pages/achievement-facades');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  Achievement/facades page has correclty loaded.')
 });
 
 // Achievment: Big works page
 app.get('/achievements/big-works', function(req, res) {
-    res.render('pages/achievement-big-works');
-    console.log('DEV-Message: Achievement us page has correclty loaded')
+	res.render('pages/achievement-big-works');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  Achievement/big-works page has correclty loaded.')
 });
 
 // Achievment: Renovations page
 app.get('/achievements/renovations', function(req, res) {
-    res.render('pages/achievement-renovations');
-    console.log('DEV-Message: Achievement us page has correclty loaded')
+	res.render('pages/achievement-renovations');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  Achievement/renovations page has correclty loaded.')
 });
 
 // Achievment: Earthworks page
 app.get('/achievements/earthworks', function(req, res) {
-    res.render('pages/achievement-earthworks');
-    console.log('DEV-Message: Achievement us page has correclty loaded')
+	res.render('pages/achievement-earthworks');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS)  --  Achievement/earthworks page has correclty loaded.')
 });
 
 // Contact page
 app.get('/contact', function(req, res) {
-    res.render('pages/contact');
-    console.log('DEV-Message: Contact page has correclty loaded')
+	res.render('pages/contact');
+	console.log('DEV-Message: [PAGE RENDERING]_(SUCCESS) --  Contact page has correclty loaded.')
 });
 
 // Contact page
 app.post('/contact', function(req, res) {
 	console.log('Data: ', req.body)
-    res.json({ message: 'Message received!'})
+	res.json({ message: 'Message received!'})
 });
 
 app.get('*', (req, res, next) => {
-	res.status(200).send('Sorry, page not found');
+	res.status(200).send('DEV-Message: [PAGE RENDERING]_(ERROR) --  Page not found');
 	next();
 });
 
-app.listen(PORT, () => {
-    console.log(`The ${ websiteName } app is running on port ${ PORT }`);
+//#############################################################################################//
+// APP LISTENING
+app.listen(port, () => {
+	console.log(`DEV-Message: [APP LISTENING]_(SUCCESS) -- ${websiteName}'s app is running on port ${port}`);
 });
